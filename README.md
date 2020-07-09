e917483dec43e6889519096a0b20e9639baecf33
Steve Falcigno <steve1281@hotmail.com>
2020-07-09 13:46:40 -0400

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Hello World
+
+This is an example project demonstrating how to publish a python module to PyPI
+
+...
+## Installation
+Run the following command to install:
+```python
+pip install helloworld
+```
+...
+## Usage
+```python
+from helloworld import say_hello
+say_hello()
+say_hello("Everybody")
+```
+
+## For developers
+
+To install helloworld, along with the tools you need to develop and run tests, run the following in your virtualenv:
+```bash
+$ pip install -e .[dev]
+```
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name='helloworld',
+  version='0.0.1',
+  description='Say hello!',
+  py_modules=["helloworld"],
+  package_dir={'': 'src'},
+  classifiers=[
+    "Programming Langauge :: Python :: 3",
+    "Programming Langauge :: Python :: 3.6",
+    "Programming Langauge :: Python :: 3.7",
+    "License :: OSI Approved :: MIT ",
+    "Operationing System :: OS Independent",
+  ],
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  install_requires = [
+    
+  ],
+  extras_require = {
+    "dev": [
+      "pytest>=3.7",
+      "check-manifest",
+    ],
+  },
+  url="https://github.com/steve1281/helloworld_package",
+  author="Steven V Falcigno",
+  author_email="steve1281@hotmail.com",
+)
+
